e1d605bb85cc4d4f11ac1c8ade8fc09d95630ee8
sinuhe Montero <sinuhem23@gmail.com>
2019-10-20 21:14:19 -0400

    updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,20 +4,20 @@
 var friendsArray = [
     {
         // 1
-        name:"Adog Ruff",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name:"Johnny Bravo",
+        photo:"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwj0-4iElazlAhXhmeAKHRqwCe4QjRx6BAgBEAQ&url=https%3A%2F%2Fvsbattles.fandom.com%2Fwiki%2FJohnny_Bravo&psig=AOvVaw2F3ZhQ1w0tOvq73LueBAiS&ust=1571706382423241",
         scores:["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
     },
     {
         // 2
         name:"Tiffany E",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        photo:"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi71-mRlazlAhUImeAKHQ8XDGUQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.allthingsdogs.com%2Fblack-pug%2F&psig=AOvVaw0YDWsK-kiPOxY3jOxfX-MM&ust=1571706497152073",
         scores:["5", "5", "2", "3", "5", "1", "6", "8", "3", "9"]
     },
     {
         // 3
-        name:"Babo Row",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name:"Doug Funny",
+        photo:"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiMhPynlazlAhUBVd8KHb2WAiUQjRx6BAgBEAQ&url=https%3A%2F%2Ftwitter.com%2Fdougepisodes&psig=AOvVaw2cWbk8nOdCeSOSZPWoSCXB&ust=1571706525558344",
         scores:[
             "5",
             "1",
@@ -33,8 +33,8 @@ var friendsArray = [
     },
     {
         // 4
-        name:"Etzuwatzu Va",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name:"Buttercup",
+        photo:"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwi6qKHSlazlAhXQl-AKHUOHAvcQjRx6BAgBEAQ&url=http%3A%2F%2Fmrwgifs.com%2Fbuttercup-eye-twitch-gif-on-the-powerpuff-girls%2F&psig=AOvVaw1Or2NhO_3i7mq9E0DrfgW0&ust=1571706597841501",
         scores:[
             "5",
             "5",
@@ -50,8 +50,8 @@ var friendsArray = [
     },
     {
         // 5
-        name:"John LEE",
-        photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        name:"Batman",
+        photo:"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjax9_ylazlAhUFo1kKHVvRBa4QjRx6BAgBEAQ&url=https%3A%2F%2Fwww.cinemablend.com%2Fnews%2F2482506%2Fthe-batman-an-updated-cast-list&psig=AOvVaw2FjrXbbhTawMIT4IvCRWwr&ust=1571706693958139",
         scores:[
             "5",
             "5",
